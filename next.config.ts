cb84f5e0f60e603f750274e4061fc12ebe8bc5f2
Allower <150520849+Allower9@users.noreply.github.com>
2025-07-16 23:24:18 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Опционально: отключает ошибки TypeScript при сборке
   },
+  module.exports = {
+  output: 'standalone', 
+}
 };
 
 export default nextConfig;
