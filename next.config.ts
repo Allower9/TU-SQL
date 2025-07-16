ef25543ed37d5da278f62146c38b577969a83b46
1-C-E <pitenevv@mail.ru>
2025-07-16 22:09:12 +0300

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Отключает проверку ESLint при сборке
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Опционально: отключает ошибки TypeScript при сборке
+  },
+};
+
+export default nextConfig;
