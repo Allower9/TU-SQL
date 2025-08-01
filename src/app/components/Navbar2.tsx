f059783e8a53d3356b7c021ddbb7e84602763f9c
Allower <150520849+Allower9@users.noreply.github.com>
2025-08-01 18:22:40 +0300

    Update Navbar2.tsx

--- src/app/components/Navbar2.tsx
+++ src/app/components/Navbar2.tsx
@@ -5,16 +5,16 @@ const Navbar2 = () => {
     <nav className="flex justify-center ">
         <div className="p-[16px] gap-[40px] flex text-black text-[18px] mt-[50px] border-[1px] border-solid border-black rounded-[30px] align-center">
           <Link href="/programs/spo">
-            Среднее профессиональное образование!
+            Среднее профессиональное образование
           </Link>
           <Link href="/programs/bachelor">
-            Бакалавриат!
+            Бакалавриат
           </Link>
           <Link href="/programs/specialty">
-            Специалитет!
+            Специалитет
           </Link>
           <Link href="/programs/magistracy">
-            Магистратура!
+            Магистратура
           </Link>
         </div>
     </nav>
